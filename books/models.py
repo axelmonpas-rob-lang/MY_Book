8cf3beb0bac272d288b9ac69a65a3a1ba67d0a8c
axelmonpas-rob-lang <axelmonpas@gmail.com>
2026-04-20 16:13:21 -0700

    paso 6

--- books/models.py
+++ books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class category(models.Model):
+    title = models.CharField(max_length=250)
+
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'Categories'
+
+
+    def __str__(self):
+        return self.title
+    
+class Book(models.Model):
+        
+        title = models.CharField(max_length=250)
+        author = models.CharField(max_length=250)
+        image = models.CharField(max_length=2000)
+        created_at =models.CharField(auto_now_add=True)
+        update_at = models.DateTimeField(auto_now=True)
