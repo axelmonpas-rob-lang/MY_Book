d13d4e31554c7e1d779bacc2a35348fc12421ded
axelmonpas-rob-lang <axelmonpas@gmail.com>
2026-04-20 18:25:50 -0700

    paso 10

---
+++ books/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import home, detail
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('books/<int:id>/', detail, name='book_details'),
+
+]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
+
+
+from .models import Book, Category
+
 
 # Create your views here.
+def home(request):
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, 'books/home.html',context)
+
+
+def detail(request, id):
+    book = get_object_or_404(book, id=id, status=Book.ACTIVE)
+ 
+    context = {
+        'book': book,
+        'form': form,
+    }
+    return render(request, 'books/detail.html', context)
