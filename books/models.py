2e4512eec70bbaa81245bec6eec4fca95506f731
axelmonpas-rob-lang <axelmonpas@gmail.com>
2026-04-20 17:09:26 -0700

    paso 8

--- books/models.py
+++ books/models.py
@@ -18,5 +18,5 @@ class Book(models.Model):
         title = models.CharField(max_length=250)
         author = models.CharField(max_length=250)
         image = models.CharField(max_length=2000)
-        created_at =models.CharField(auto_now_add=True)
+        created_at =models.DateTimeField(auto_now_add=True)
         update_at = models.DateTimeField(auto_now=True)
